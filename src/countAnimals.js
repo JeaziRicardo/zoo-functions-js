99c890bc5307b431b8f1a9e46d5be1f11ce52735
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-03-16 20:55:29 -0300

    (Requisito 5 - Implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce(((acc, specie) => {
+      acc[`${specie.name}`] = specie.residents.length;
+      return acc;
+    }), {});
+  }
+  if (!animal.sex) {
+    return data.species
+      .find((specie) => specie.name === animal.specie)
+      .residents.length;
+  }
+  return data.species
+    .find((specie) => specie.name === animal.specie)
+    .residents.filter((animalResid) => animalResid.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
