5f1c5a92a3f011df6b7b4767e06178c3fc7b37f1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-03-16 16:29:29 -0300

    (Requisito 2 - Implementa a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .some((specie) => specie.name === animal && specie.residents
+      .every((animals) => animals.age >= age));
 }
 
 module.exports = getAnimalsOlderThan;
