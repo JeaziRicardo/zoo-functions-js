bee1bcfaa6a33891b10ba8640fef305f82be4dcc
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-03-17 13:14:02 -0300

    (Requisito 6) - Implementa a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { adult, child, senior } = data.prices;
+
 function countEntrants(entrants) {
-  //  seu código aqui
+  return entrants.reduce((acc, entrant) => {
+    if (entrant.age < 18) {
+      acc.child += 1;
+      return acc;
+    }
+    if (entrant.age >= 50) {
+      acc.senior += 1;
+      return acc;
+    }
+    acc.adult += 1;
+    return acc;
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const objEntrants = countEntrants(entrants);
+  return (objEntrants.adult * adult)
+    + (objEntrants.child * child)
+    + (objEntrants.senior * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
