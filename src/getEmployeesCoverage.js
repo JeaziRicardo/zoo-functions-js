36e95769358e44b0e36a91820c10aedefb77e948
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-03-19 22:57:57 -0300

    (Requisito 10) - Implementa a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function findEmployee(obj) {
+  const objEmployee = data.employees
+    .find((employee) => obj.name === employee.firstName
+    || obj.name === employee.lastName
+    || obj.id === employee.id);
+  if (!objEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  return objEmployee;
+}
+
+function objEmployees(obj) {
+  const objName = {
+    id: findEmployee(obj).id,
+    fullName: `${findEmployee(obj).firstName} ${findEmployee(obj).lastName}`,
+    species: findEmployee(obj).responsibleFor.map((id) => data.species
+      .find((specie) => id === specie.id).name),
+    locations: findEmployee(obj).responsibleFor.map((id) => data.species
+      .find((specie) => id === specie.id).location),
+  };
+  return objName;
+}
+
+function getEmployeesCoverage(obj) {
+  if (!obj) {
+    return data.employees.map((employee) => objEmployees(employee));
+  }
+  if (obj.name || obj.id) {
+    return objEmployees(obj);
+  }
 }
 
 module.exports = getEmployeesCoverage;
